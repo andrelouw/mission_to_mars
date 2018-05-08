9b7b789fe9ca965abc59d4adeb697001856403ef
Andre Louw <alouw@retrorabbit.co.za>
2018-05-08 18:42:30 +0200

    Add Rover class.
    Very basic class that only has constructor for now.
    Also printing out the name of the rover in main to get things going.

--- mission/mission.py
+++ mission/mission.py
@@ -1,2 +1,6 @@
+from mission.rover import Rover
+
 if __name__ == '__main__':
     print("Welcome to the Mission to mars!")
+    rover = Rover()
+    print(f"Rover name is: {rover.name}")
---
+++ mission/rover.py
@@ -0,0 +1,8 @@
+class Rover:
+    def __init__(self):
+        self.name = "athena"
+        self.position_x = 0
+        self.position_y = 0
+        self.direction = 'N'  # TODO: maybe make this enum?
+        self.max_x = 10
+        self.max_y = 10
