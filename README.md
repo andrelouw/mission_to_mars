0c5f236185699e53b356f4551a1217b3325fde96
Andre Louw <alouw@retrorabbit.co.za>
2018-05-08 18:11:02 +0200

    Setup project with necessary project files.
    These Files include:
    - The mission module and mission.test module.
    - Print welcom message to the console
    - setup.py file
    - License
    - Some info to run the project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mission_to_mars
 The Mars Rover Technical Challenge
+
+## Launch
+To launch the mission, open up the terminal and run:
+```python -m mission.mission```
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='MissionToMars',
+    version='0.1.0',
+    author='A. Louw',
+    author_email='louwandre90@gmail.com',
+    packages=['mission', 'mission.test'],
+    license='LICENSE.txt',
+    description='The Mars Rover Technical Challenge',
+    long_description=open('README.txt').read(),
+    install_requires=[
+    ],
+)
